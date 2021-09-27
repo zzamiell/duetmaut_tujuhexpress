e1702c34898a05ab7ca2693f963da540152dca25
Anugrah abdi kautsar <32447203+zzamiell@users.noreply.github.com>
2021-09-27 10:56:45 +0700

    cek sckript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -127,13 +127,13 @@
   <!--  Google Maps Plugin    -->
   {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> --}}
   <!-- Chart JS -->
-  {{-- <script src="{{ asset('assets') }}/js/plugins/chartjs.min.js"></script> --}}
+  <script src="{{ asset('assets') }}/js/plugins/chartjs.min.js"></script>
   <!--  Notifications Plugin    -->
   {{-- <script src="{{ asset('assets') }}/js/plugins/bootstrap-notify.js"></script> --}}
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  {{-- <script src="{{ asset('assets') }}/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script> --}}
+  <script src="{{ asset('assets') }}/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
   <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-  {{-- <script src="{{ asset('assets') }}/demo/demo.js"></script> --}}
+  <script src="{{ asset('assets') }}/demo/demo.js"></script>
 
   @include('layouts.script')
   @stack('js')
